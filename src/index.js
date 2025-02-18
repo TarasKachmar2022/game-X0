e9e2e904f16290a2751eff087116a906d4a0df80
Taras <kachmar.taras@gmail.com>
2025-02-18 22:37:47 +0200

    add game logic

---
+++ src/index.js
@@ -0,0 +1,86 @@
+const fieldEl = document.querySelector('.field');
+const winnerEl = document.querySelector('.winner');
+
+fieldEl.addEventListener('click', onClick);
+
+let player = 'X';
+let historyx = [];
+let history0 = [];
+
+const wins = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [7, 4, 1],
+  [8, 5, 2],
+  [9, 6, 3],
+  [7, 5, 3],
+  [9, 5, 1],
+];
+console.log(typeof wins);
+console.log(typeof historyx);
+
+function createMarkup() {
+  let markup = '';
+
+  for (let i = 1; i < 10; i += 1) {
+    markup += `<div class="item" data-id="${i}"></div>`;
+  }
+
+  for (let i = 1; i < 9; i += 1) {
+    markup += `<div class="line" data-line="${i}"></div>`;
+  }
+
+  fieldEl.innerHTML = markup;
+}
+
+createMarkup();
+
+function onClick(e) {
+  const { target } = e;
+
+  console.log(e.currentTarget);
+  console.log(target);
+
+  if (target.textContent) {
+    return;
+  }
+
+  target.textContent = player;
+  const id = Number(target.dataset.id);
+
+  if (player === 'X') {
+    historyx.push(id);
+  } else {
+    history0.push(id);
+  }
+
+  console.log(history0);
+  console.log(historyx);
+
+  player = player === 'X' ? '0' : 'X';
+
+  if (historyx.length + history0.length === 9) {
+    reset();
+  }
+
+  checkWins();
+}
+
+function reset() {
+  createMarkup();
+  historyx = [];
+  history0 = [];
+  player = 'X';
+}
+
+function checkWins() {
+  if (historyx.length + history0.length >= 5) {
+    const isWinner = wins.some(item => item.every(id => historyx.includes(id)));
+    console.log(isWinner);
+    if (isWinner) {
+      console.log('win');
+      winnerEl.textContent = 'Winner';
+    }
+  }
+}
